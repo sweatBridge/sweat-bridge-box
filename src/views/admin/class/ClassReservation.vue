972ff47305a847cff115fdbdcf94db0d83436d12
daniel.hyuns <daniel.hyuns@kakaocorp.com>
2023-08-12 19:19:19 +0900

    feat: 수업 등록 페이지에 FullCalendar 템플릿 추가
    #feature/SWEAT-15

--- src/views/admin/class/ClassReservation.vue
+++ src/views/admin/class/ClassReservation.vue
@@ -3,10 +3,17 @@
     <CCol>
       <CCard>
         <CCardHeader>
-          <strong>리타게팅 대상 트래커 등록</strong>
+          <strong>수업 등록</strong>
         </CCardHeader>
         <CCardBody>
-          <FullCalendar :options="calendarOptions" />
+          <div class="demo-app-main">
+            <FullCalendar class="demo-app-calendar" :options="calendarOptions">
+              <template v-slot:eventContent="arg">
+                <b>{{ arg.timeText }}</b>
+                <i>{{ arg.event.title }}</i>
+              </template>
+            </FullCalendar>
+          </div>
         </CCardBody>
       </CCard>
     </CCol>
@@ -16,20 +23,91 @@
 <script>
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-export default {
-  name: 'ClassReservation',
+import { INITIAL_EVENTS, createEventId } from './event-utils'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   components: {
     FullCalendar,
   },
-  setup() {
-    const calendarOptions = {
-      plugins: [dayGridPlugin, interactionPlugin],
-      initialView: 'dayGridMonth',
-    }
+  data() {
     return {
-      calendarOptions,
+      calendarOptions: {
+        plugins: [
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin, // needed for dateClick
+        ],
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        },
+        initialView: 'dayGridMonth',
+        initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+        editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        select: this.handleDateSelect,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents,
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
+      },
+      currentEvents: [],
     }
   },
-}
+  methods: {
+    handleWeekendsToggle() {
+      this.calendarOptions.weekends = !this.calendarOptions.weekends // update a property
+    },
+    handleDateSelect(selectInfo) {
+      let title = prompt('Please enter a new title for your event')
+      let calendarApi = selectInfo.view.calendar
+
+      calendarApi.unselect() // clear date selection
+
+      if (title) {
+        calendarApi.addEvent({
+          id: createEventId(),
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay,
+        })
+      }
+    },
+    handleEventClick(clickInfo) {
+      console.log(clickInfo)
+    },
+    handleEvents(events) {
+      this.currentEvents = events
+    },
+  },
+})
 </script>
+
+<style scoped lang="scss">
+.demo-app {
+  display: flex;
+  min-height: 100%;
+  font-family:
+    Arial,
+    Helvetica Neue,
+    Helvetica,
+    sans-serif;
+  font-size: 14px;
+}
+
+.demo-app-main {
+  flex-grow: 1;
+  padding: 3em;
+}
+</style>
