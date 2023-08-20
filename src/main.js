ced8c7293d1d906a63d1bdc57728c5dfe1b6334b
daniel.hyuns <daniel.hyuns@kakaocorp.com>
2023-08-20 15:30:47 +0900

    feat: vue3-easy-data-table 추가
    #feature/SWEAT-16

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
 import CoreuiVue from '@coreui/vue'
 import CIcon from '@coreui/icons-vue'
@@ -15,5 +17,6 @@ app.use(CoreuiVue)
 app.provide('icons', icons)
 app.component('CIcon', CIcon)
 app.component('DocsExample', DocsExample)
+app.component('EasyDataTable', Vue3EasyDataTable)
 
 app.mount('#app')
