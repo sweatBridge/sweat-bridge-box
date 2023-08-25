2522224db2b92416a82cecd4f96bc5061f156e37
daniel.hyuns <daniel.hyuns@kakaocorp.com>
2023-08-25 16:00:31 +0900

    feat: v-calendar library 추가 #feature/SWEAT-16

--- src/main.js
+++ src/main.js
@@ -9,14 +9,20 @@ import CoreuiVue from '@coreui/vue'
 import CIcon from '@coreui/icons-vue'
 import { iconsSet as icons } from '@/assets/icons'
 import DocsExample from '@/components/DocsExample'
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
+import 'v-calendar/style.css'
+
 
 const app = createApp(App)
 app.use(store)
 app.use(router)
 app.use(CoreuiVue)
+app.use(setupCalendar, {})
 app.provide('icons', icons)
 app.component('CIcon', CIcon)
 app.component('DocsExample', DocsExample)
 app.component('EasyDataTable', Vue3EasyDataTable)
+app.component('VCalendar', Calendar)
+app.component('VDatePicker', DatePicker)
 
 app.mount('#app')
