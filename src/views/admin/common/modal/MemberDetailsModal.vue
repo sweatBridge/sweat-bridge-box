13fafbf201f44901b9131c6f2eb1ecdf6213685b
daniel.hyuns <daniel.hyuns@kakaocorp.com>
2023-09-17 09:02:37 +0900

    feat: 와드 히스토리 관련 모듈 추가
    #feature/SWEAT-16

---
+++ src/views/admin/common/modal/MemberDetailsModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Member Detail Modal</p>
+
+</template>
+
+<script>
+export default {
+  name: "MemberDetailsModal"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/admin/workout/MemberRecord.vue
@@ -0,0 +1,59 @@
+<template>
+  <EasyDataTable
+    :headers="headers"
+    :items="records"
+    table-class-name="customize-table"
+  >
+    <template #item-name="{ name }">
+      {{name}}
+    </template>
+    <template #item-level="{ level }">
+      {{level}}
+    </template>
+    <template #item-record="{ record }">
+      {{record}}
+    </template>
+    <template #item-detail="">
+      <CButton
+        color="light"
+        size="sm"
+      >
+        <CIcon name="cil-notes" />
+      </CButton>
+    </template>
+  </EasyDataTable>
+
+</template>
+
+<script>
+export default {
+  name: "MemberRecord",
+  components: {},
+  props: {
+    records: {
+      type: Object,
+      required: true,
+    }
+  },
+  setup() {
+    const headers = [
+      { text: "이름", value: "name", width: "80" },
+      { text: "난이도", value: "level" },
+      { text: "기록", value: "record" },
+      { text: "상세", value: "detail" },
+    ]
+
+    return {
+      headers,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.customize-table {
+  /*--easy-table-header-background-color: #53651e;*/
+  /*--easy-table-header-font-color: rgba(255, 255, 255, 0.38);*/
+}
+
+</style>
