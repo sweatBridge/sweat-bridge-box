2961abf8f8a248c304b270f9905fe7285fdfdf61
DaehyunKim <daniel8926@gmail.com>
2024-07-27 21:22:12 +0900

    feat: AppHeader 수정
    - Sidebar 제거
    - Header 구성 변경

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,25 +1,34 @@
 <template>
   <CHeader position="sticky" class="mb-4">
     <CContainer fluid>
-      <CHeaderToggler class="ps-1" @click="$store.commit('toggleSidebar')">
+      <!-- <CHeaderToggler class="ps-1" @click="$store.commit('toggleSidebar')">
         <CIcon icon="cil-menu" size="lg" />
-      </CHeaderToggler>
+      </CHeaderToggler> -->
       <CHeaderBrand class="mx-auto d-lg-none" to="/">
         <CIcon :icon="logo" height="48" alt="Logo" />
       </CHeaderBrand>
       <CHeaderNav class="d-none d-md-flex me-auto">
         <CNavItem>
-          <CNavLink href="#/admin/registered-wod-list"> Wod </CNavLink>
+          <CNavLink href="#/admin/registered-wod-list">
+            <CIcon class="mx-2" icon="cil-list" size="md" />
+            와드
+          </CNavLink>
         </CNavItem>
         <CNavItem>
-          <CNavLink href="#/admin/member"> Member </CNavLink>
+          <CNavLink href="#/admin/member">
+            <CIcon class="mx-2" icon="cil-user" size="md" />
+            회원
+          </CNavLink>
         </CNavItem>
         <CNavItem>
-          <CNavLink href="#/admin/reservation"> Class </CNavLink>
+          <CNavLink href="#/admin/reservation">
+            <CIcon class="mx-2" icon="cil-calendar" size="md" />
+            수업
+          </CNavLink>
         </CNavItem>
       </CHeaderNav>
       <CHeaderNav>
-        <CNavItem>
+        <!-- <CNavItem>
           <CNavLink href="#/admin/registered-wod-list">
             <CIcon class="mx-2" icon="cil-bell" size="lg" />
           </CNavLink>
@@ -33,11 +42,23 @@
           <CNavLink href="#/admin/registered-wod-list">
             <CIcon class="mx-2" icon="cil-envelope-open" size="lg" />
           </CNavLink>
-        </CNavItem>
+        </CNavItem> -->
         <CNavItem>
           <CInputGroupText id="basic-addon3">{{boxName}}</CInputGroupText>
         </CNavItem>
         <AppHeaderDropdownAccnt />
+        <CNavItem>
+          <CNavLink href="#/admin/my-page">
+            <CIcon class="mx-2" icon="cil-settings" size="md" />
+            관리자 설정
+          </CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink @click="handleLogOutClick">
+            <CIcon class="mx-2" icon="cil-lock-locked" size="md" />
+            로그아웃
+          </CNavLink>
+        </CNavItem>
       </CHeaderNav>
     </CContainer>
     <CHeaderDivider />
@@ -45,25 +66,53 @@
       <AppBreadcrumb />
     </CContainer>
   </CHeader>
+  <toast-message ref="toastMessageRef" />
 </template>
 
 <script>
 import AppBreadcrumb from './AppBreadcrumb'
 import AppHeaderDropdownAccnt from './AppHeaderDropdownAccnt'
+import ToastMessage from "@/views/admin/common/toast/ToastMessage.vue";
 import { logo } from '@/assets/brand/logo'
 import {useStore} from "vuex";
-import {computed} from "vue";
+import {computed, ref} from "vue";
+import router from '@/router';
 export default {
   name: 'AppHeader',
   components: {
     AppBreadcrumb,
     AppHeaderDropdownAccnt,
+    ToastMessage
   },
   setup() {
     const store = useStore()
+    const toastMessageRef = ref(null)
+    const handleLogOutClick = () => {
+      try {
+        store.dispatch('logout')
+        toastMessageRef.value.createToast({
+          title: '성공',
+          content: '로그아웃 성공',
+          type: 'success'
+        });
+        store.commit('SET_BOX_STATE_EMPTY')
+        setTimeout(() => {
+          router.push("/pages/login")
+        }, 1000)
+      } catch (error) {
+        console.log('로그아웃 실패 : ' + error.meessage)
+        toastMessageRef.value.createToast({
+          title: '실패',
+          content: '로그아웃 실패',
+          type: 'danger'
+        })
+      }
+    }
     return {
       logo,
-      boxName: computed(() => store.state.account.boxState.boxName)
+      boxName: computed(() => store.state.account.boxState.boxName),
+      toastMessageRef,
+      handleLogOutClick,
     }
   },
 }
