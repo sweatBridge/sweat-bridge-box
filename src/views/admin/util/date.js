1e67d4ded491f0aacec76e1c563fbca5322c2efd
DaehyunKim <daniel8926@gmail.com>
2024-08-24 18:06:04 +0900

    refactor: 수업 관리 모달 시간 포맷 변경 & 시간 포맷 함수 common으로 이동

--- src/views/admin/util/date.js
+++ src/views/admin/util/date.js
@@ -5,3 +5,15 @@ export function extractDateInKorean(dateStr) {
   const day = date.getDate()
   return `${month}월 ${day}일`
 }
+
+export function formatDateTime(isoString) {
+  const date = new Date(isoString);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+}
